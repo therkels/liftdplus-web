bfc675c52fd70ed7a3f03ffc3a818cdd3a6e2c5d
Jakob Louis Therkelsen <therkels@msu.edu>
2025-01-21 09:43:18 -0500

    update test

--- liftdplus-web/app/index.tsx
+++ liftdplus-web/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>how fast is this?</Text>
     </View>
   );
 }
